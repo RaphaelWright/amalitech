6f26be64270c1cb699d1f2d7393f658cd4a7cd39
RaphaelWright <raphaelwright2003@gmail.com>
2024-09-20 15:59:28 +0000

    significant progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-
-import SignUp from './components/SignUp'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <>
-      <SignUp></SignUp>
-    </>
+    <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    </div>
   )
       
 }
